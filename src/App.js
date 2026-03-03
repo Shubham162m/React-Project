35ce44504c3f61bab70048a3c3b2c2da5fa2d17c
MODI SHUBHAM <103481632+Shubham162m@users.noreply.github.com>
2026-03-03 21:29:42 +0530

    Refactor App.js to clean up imports and comments
    Refactor App.js by removing unused imports and commented-out code.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Sidebar from './components/Sidebar';
-import Main from './components/Main';
-import Child from './component/Child';
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+// import Sidebar from './components/Sidebar';
+// import Main from './components/Main';
+// import Child from './component/Child';
+import UserData from './PropsMax/Database';
+import ClassStateDemo from './state/ClassStateDemo';
+import FunctionalStateDemo from './state/FunctionalStateDemo';
+
+
 
 function App()
 {
   return (
 
 
-       <div className="App"><Navbar /><Sidebar/>
-      <div className="content"><Main /></div>
-      <div className="child"><Child/></div>
-      <Footer /></div>  
-  );
-}
+      //  <div className="App"><Navbar /><Sidebar/>
+      // <div className="content"><Main /></div>
+      // <div className="child"><Child/></div>
+      // <Footer /></div>  
+<div>
+    
+  <UserData/>
+  <A/>
+  <hr/>
+  <ClassStateDemo/>
+  <hr/>
+  <FunctionalStateDemo/>
+</div>
 
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
-*/
 export default App;
 
